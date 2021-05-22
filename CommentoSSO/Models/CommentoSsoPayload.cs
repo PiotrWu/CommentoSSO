3a4eef4744bd275cc108aaecd49cb334baef326b
PiotrWu <piotr.welfler@outlook.com>
2021-05-22 20:39:54 +0200

    CommentoSSO - introduced CommentoSsoPayload model

---
+++ CommentoSSO/Models/CommentoSsoPayload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommentoSSO.Models
+{
+    public class CommentoSsoPayload
+    {
+        public int token { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+        public string photo { get; set; }
+
+    }
+}
