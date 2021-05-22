f538275d03eaf1c16145beb0b2e7654997986e26
PiotrWu <piotr.welfler@outlook.com>
2021-05-22 21:13:31 +0200

    Introduced HomeController_Tests class

---
+++ CommentoSSO.Tests/HomeController_Tests.cs
@@ -0,0 +1,23 @@
+﻿using CommentoSSO.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommentoSSO.Tests
+{
+    public class HomeController_Tests
+    {
+        [Fact]
+        public void ShouldReturnDecodedStringFromHex()
+        {
+            var hex = "48656C6C6F20576F726C64";
+            var expected = "Hello World";
+            var result = HomeController.FromHexString(hex);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
