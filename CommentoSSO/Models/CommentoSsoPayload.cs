8fc55a8c021694f257e5c982f1343f4ffeaeef7d
PiotrWu <piotr.welfler@outlook.com>
2021-05-22 20:56:47 +0200

    CommentoSSO - added ToJson method

--- CommentoSSO/Models/CommentoSsoPayload.cs
+++ CommentoSSO/Models/CommentoSsoPayload.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,11 +8,16 @@ namespace CommentoSSO.Models
 {
     public class CommentoSsoPayload
     {
-        public int token { get; set; }
+        public string token { get; set; }
         public string email { get; set; }
         public string name { get; set; }
         public string link { get; set; }
         public string photo { get; set; }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
 
     }
 }
