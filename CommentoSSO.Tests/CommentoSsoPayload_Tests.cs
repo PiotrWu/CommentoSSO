d8eb50aab3501fc2088a58a3003b75edcad0a6eb
PiotrWu <piotr.welfler@outlook.com>
2021-05-22 20:57:54 +0200

    CommentoSSO - introduced CommentoSsoPayload ToJson method Unit test

---
+++ CommentoSSO.Tests/CommentoSsoPayload_Tests.cs
@@ -0,0 +1,26 @@
+﻿using CommentoSSO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommentoSSO.Tests
+{
+    public class CommentoSsoPayload_Tests
+    {
+        [Fact]
+        public void ShouldReturnJson()
+        {
+            CommentoSsoPayload commentoSsoPayload = new CommentoSsoPayload()
+            {
+                token = "ABC", name = "test", email = "email@email.com", link = "someLink", photo = "url"
+            };
+            var actual = commentoSsoPayload.ToJson();
+            string expected = "{\"token\":\"ABC\",\"email\":\"email@email.com\",\"name\":\"test\",\"link\":\"someLink\",\"photo\":\"url\"}";
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
