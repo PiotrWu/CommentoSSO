2cd1d476fd5d0685374d5a1cf383e47d55581f01
PiotrWu <piotr.welfler@outlook.com>
2021-05-23 10:44:19 +0200

    HomeController - added decoding and encoding methods

--- CommentoSSO/Controllers/HomeController.cs
+++ CommentoSSO/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 ﻿using CommentoSSO.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,7 +36,10 @@ namespace CommentoSSO.Controllers
         [Route("Sso")]
         public IActionResult Sso(string token, string hmac)
         {
+            
+
             CommentoSsoRequest commentoSsoRequest = new CommentoSsoRequest(token, hmac);
+
             return View(commentoSsoRequest);
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -41,17 +48,46 @@ namespace CommentoSSO.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-
         public static string FromHexString(string hexString)
         {
-            string stringValue = "";
-            for (int i = 0; i < hexString.Length / 2; i++)
+            byte[] dBytes = StringToByteArray(hexString);
+            string utf8result = System.Text.Encoding.ASCII.GetString(dBytes);
+            return utf8result;
+        }
+
+        private static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+
+        public static string HashHMACHex(string keyHex, string message)
+        {
+            byte[] hash = HashHMAC(HexDecode(keyHex), HexDecode(message));
+            return HashEncode(hash);
+        }
+
+        private static byte[] HexDecode(string hex)
+        {
+            var bytes = new byte[hex.Length / 2];
+            for (int i = 0; i < bytes.Length; i++)
             {
-                string hexChar = hexString.Substring(i * 2, 2);
-                int hexValue = Convert.ToInt32(hexChar, 16);
-                stringValue += Char.ConvertFromUtf32(hexValue);
+                bytes[i] = byte.Parse(hex.Substring(i * 2, 2), NumberStyles.HexNumber);
             }
-            return stringValue;
+            return bytes;
+        }
+        private static string HashEncode(byte[] hash)
+        {
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+
+        private static byte[] HashHMAC(byte[] key, byte[] message)
+        {
+            var hash = new HMACSHA256(key);
+            return hash.ComputeHash(message);
         }
     }
 }
