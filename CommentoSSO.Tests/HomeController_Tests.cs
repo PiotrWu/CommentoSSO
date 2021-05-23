9ad396bd4cb6cc88049a1c6e64b236f48cd5a040
PiotrWu <piotr.welfler@outlook.com>
2021-05-23 10:45:16 +0200

    HomeController - added HMAC test method

--- CommentoSSO.Tests/HomeController_Tests.cs
+++ CommentoSSO.Tests/HomeController_Tests.cs
@@ -19,5 +19,19 @@ namespace CommentoSSO.Tests
 
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void ShouldReturnExpectedCalculatedHmac()
+        {
+            string message = "e815a338336b14cdcf734052310f8aba48faad3e80bb8e723453ac545b800531";
+            string expectedHex = "b23cc27b9d5bdffbd9c09bf50be3edfd3d764edcb7885f8a7daec989219d1169";
+            string key = "d743c678e26b725c6c1fb4941a02f8c124583dd72b54cb59c4c39f43826d75ea";
+            string result = HomeController.HashHMACHex(key, message);
+
+          
+
+            Assert.Equal(expectedHex, result);
+
+        }
     }
 }
